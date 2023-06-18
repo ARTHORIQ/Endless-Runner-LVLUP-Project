b6395189f17505b5c649f11ff1a107fbef6a89a9
Sigiaa <yours.patrick@gmail.com>
2023-06-18 15:34:50 +0700

    Disabling Health manager

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class HealthManager : MonoBehaviour
-{
-     public GameObject[] hearts;
+// public class HealthManager : MonoBehaviour
+// {
+//      public GameObject[] hearts;
 
-    private int playerHealth = 3;
+//     private int playerHealth = 3;
 
-    private void Start()
-    {
-        UpdateHeartDisplay();
-    }
+//     private void Start()
+//     {
+//         UpdateHeartDisplay();
+//     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            ReduceHealth();
-        }
-    }
+//     private void OnCollisionEnter(Collision collision)
+//     {
+//         if (collision.gameObject.CompareTag("Obstacle"))
+//         {
+//             ReduceHealth();
+//         }
+//     }
 
-    private void ReduceHealth()
-    {
-        playerHealth--;
-        if (playerHealth < 0)
-        {
-            playerHealth = 0;
-            return;
-        }
-        UpdateHeartDisplay();
-    }
+//     private void ReduceHealth()
+//     {
+//         playerHealth--;
+//         if (playerHealth < 0)
+//         {
+//             playerHealth = 0;
+//             return;
+//         }
+//         UpdateHeartDisplay();
+//     }
 
 
-    private void UpdateHeartDisplay()
-    {
-        for (int i = 0; i < hearts.Length; i++)
-        {
-            hearts[i].SetActive(i < playerHealth);
-        }
-    }
-}
+//     private void UpdateHeartDisplay()
+//     {
+//         for (int i = 0; i < hearts.Length; i++)
+//         {
+//             hearts[i].SetActive(i < playerHealth);
+//         }
+//     }
+// }
